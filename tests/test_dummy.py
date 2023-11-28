d8aa3b8ed4244da3aadd858afad0c0ee7dd9c34f
Andrés Ferreiro González <andresoferreiro@gmail.com>
2023-11-28 19:14:02 +0100

    Add dummy test

---
+++ tests/test_dummy.py
@@ -0,0 +1,17 @@
+"""
+test_dummy.py
+
+Project: Advent of Code 2023
+
+Maintainer Andrés Ferreiro González (aferreiro@gradiant.org)
+Created @ 28/11/23 17:38:28.862000
+
+Copyright (c) 2023 Centro Tecnolóxico de Telecomunicacións de Galicia (GRADIANT)
+All Rights Reserved
+"""
+
+
+def test_dummy() -> None:
+    """Dummy test for dev environment setup purposes."""
+
+    assert True
