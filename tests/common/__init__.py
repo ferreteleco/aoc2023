3c476f0a7818ab95c352a430083b825b87bc6a8e
Andrés Ferreiro González <andresoferreiro@gmail.com>
2023-12-01 18:22:44 +0100

    Update tests structure

--- tests/common/__init__.py
+++ tests/common/__init__.py
@@ -4,7 +4,7 @@ __init__.py
 Project: Advent of Code 2023
 
 Maintainer Andrés Ferreiro González (andres.ferreiro.glez@gmail.com)
-Created @ 29/11/23 10:58:37.516000
+Created @ 29/11/23 10:58:37.512000
 
 Copyright (c) 2023 Andrés Ferreiro González
 See project license for more info
